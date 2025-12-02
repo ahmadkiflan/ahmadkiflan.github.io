3504b96f3de2dd9b4b7542fcde12b629d8d89dab
ahmadkiflan <ahmadkiflan2020@gmail.com>
2025-12-02 19:21:35 +0700

    Mengubah dan menambahkan elemen strong pada isi konten dalam modal di file style.css dan script.js

--- script.js
+++ script.js
@@ -60,18 +60,20 @@ const showMovieDetail = (movie) =>
     </figure>
     <section class="modal-content">
       <h4>Title: ${movie.Title}</h4>
-      <p>Year: ${movie.Year}</p>
-      <p>Rated: ${movie.Rated}</p>
-      <p>Released: ${movie.Released}</p>
-      <p>Runtime: ${movie.Runtime}</p>
-      <p>Genre: ${movie.Genre}</p>
-      <p>Director: ${movie.Director}</p>
-      <p>Writer: ${movie.Writer}</p>
-      <p>Actors: ${movie.Actors}</p>
-      <p>Plot: ${movie.Plot}</p>
-      <p>Language: ${movie.Language}</p>
-      <p>Country: ${movie.Country}</p>
-      <p>imdbRating: ${movie.imdbRating}</p>
-      <p>Type: ${movie.Type}</p>
+      <p>
+        <strong>Year</strong>: ${movie.Year}
+      </p>
+      <p><strong>Rated</strong>: ${movie.Rated}</p>
+      <p><strong>Released</strong>: ${movie.Released}</p>
+      <p><strong>Runtime</strong>: ${movie.Runtime}</p>
+      <p><strong>Genre</strong>: ${movie.Genre}</p>
+      <p><strong>Director</strong>: ${movie.Director}</p>
+      <p><strong>Writer</strong>: ${movie.Writer}</p>
+      <p><strong>Actors</strong>: ${movie.Actors}</p>
+      <p><strong>Plot</strong>: ${movie.Plot}</p>
+      <p><strong>Language</strong>: ${movie.Language}</p>
+      <p><strong>Country</strong>: ${movie.Country}</p>
+      <p><strong>imdbRating</strong>: ${movie.imdbRating}</p>
+      <p><strong>Type</strong>: ${movie.Type}</p>
     </section>
   </article>`;
